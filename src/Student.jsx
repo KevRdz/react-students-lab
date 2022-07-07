e89eb90a21283c5df24362ba60bf72c92b274b78
Kevin <kevin.rodriguez678@gmail.com>
2022-07-07 00:04:36 -0400

    edit score file

--- src/Student.jsx
+++ src/Student.jsx
@@ -1,11 +1,12 @@
 import StudentCard from "./StudentCard";
+import Score from "./Score";
 
 const  Student = (props) => {
   return ( 
     <>
     {/* <h1>Student List</h1> */}
     {props.stu.map(student =>
-      <StudentCard key={student.id} student={student} />
+      <StudentCard student={student} />
     )}
     </>
   );
